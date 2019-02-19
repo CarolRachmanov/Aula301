c64d53172ceb061325774109a3ddbd5fe734ad1a
Carol D <caroldantasxd@gmail.com>
2019-02-19 20:50:23 -0300

    Iniciar exercícios em python

---
+++ variables.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Exercício 1
+
+# valor1= float(input("digite o valor 1: "))
+# valor2= float(input ("digite o valor 2: "))
+# soma = valor1 + valor2
+# subtracao = valor1 - valor2
+# multiplicacao= valor1*valor2
+# divisao = valor1/valor2
+# resto = valor1%valor2
+# media = (valor1+valor2)/2
+
+# print (soma)
+# print (subtracao)
+# print (multiplicacao)
+# print (divisao)
+# print (resto)
+# print (media)
+
+#Exercício 2
+
+nome = input('Escreva seu nome: ')
+idade = int(input('Qual é a sua idade: '))
+
+if idade >= 18:
+    print ('seja bem vindo(a) ' + nome)
+
+if idade <= 11:
+    print ('Você é jovem demais para acessar o site.')
+
+else: 
+    print ('Menor de idade.')
+
+
